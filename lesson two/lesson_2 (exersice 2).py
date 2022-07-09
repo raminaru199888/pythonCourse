fbc311400ca6f9673eb89decdb1040fed74221af
raminaru199888 <108360662+raminaru199888@users.noreply.github.com>
2022-07-09 14:05:26 +0300

    Update lesson_2 (exersice 2).py

--- lesson two/lesson_2 (exersice 2).py
+++ lesson two/lesson_2 (exersice 2).py
@@ -2,6 +2,12 @@
 # и только одно число повторяется 1 раз. Найти число, которое повторяется 1 раз.
 
 
-
+number = input('Введите числа: ').split()
+list_number = []
+for i in range(len(number)):
+    list_number.append(number[i])
+for k in list_number:
+    if list_number.count(k) == 1:
+        print('Не повторяющееся число: ', k)
 
 
